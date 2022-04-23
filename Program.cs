b1fc22272eec209a121e7817354055c0ad667afd
Ivan Y <ivany@subetuweb.site>
2022-04-22 23:33:47 -0500

    main thread and empty form

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace weighting_soft
+{
+    internal class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        
+        [STAThread()]
+        static void Main()
+        {
+            SplashScreen splashScreen = new SplashScreen();
+            splashScreen.ShowDialog();
+        }
+
+    }
+}
