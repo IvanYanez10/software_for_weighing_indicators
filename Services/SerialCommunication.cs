1fe40b37660c57ebc3a3a57f8486f2c6e4e9f0ff
Ivan Y <ivany@subetuweb.site>
2022-04-26 19:28:05 -0500

    Serial communication service

---
+++ Services/SerialCommunication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weighting_soft.Services
+{
+    internal class SerialCommunication
+    {
+    }
+}
